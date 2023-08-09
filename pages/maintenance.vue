1f2701ca78c6bb6ae63a2452ace3f6faa0f9f126
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-08-09 23:06:35 +0300

    chore: maintenance page text

--- pages/maintenance.vue
+++ pages/maintenance.vue
@@ -4,7 +4,7 @@
             <div class="hero-content text-center text-base-300 dark:text-secondary">
             <div class="max-w-md">
                 <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                <p class="mb-5">Thanks for your interest! But our site is currently under maintenance, but do come back later.</p>
+                <p class="mb-5">Thanks for your interest! But our site is currently under maintenance so all features and pages have been disabled, but do come back later.</p>
             </div>
         </div>
     </div>

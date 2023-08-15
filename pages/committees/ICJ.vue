ae887276e836a036c4573125f265404b3a8e70ee
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-08-15 14:43:36 +0300

    feat: add icj page content

--- pages/committees/ICJ.vue
+++ pages/committees/ICJ.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-
+        <Committee v-bind="committee" />
     </div>
 </template>
 
 <script setup lang="ts">
-
+const committee = {
+    slug: 'icj - cij',
+    name: 'International Court of Justice - Cour internationale de justice',
+    agenda: [
+        'The Gambia v. Myanmar case'
+    ],
+    text: 'The International Court of Justice (abbreviated as ICJ) is one of the six principal organs of the United Nations acting as a interstate court or as some refer a “World Court”. It is to settle disputes between states in accordance with international law provides advisory opinions on international legal issues. The Court is the only international court that adjudicates general disputes between countrie rulings and opinions serving as primary sources of international law.\n\n The Rohingya are a Muslim Indo-Aryan ethnic minority facing mass persecution and ethnic cleansing in Myanmar in recent years. Their persecution has been commonly described as a genocide. The Myanma government deems the Rohingya as illegal immigrants and describes the conflict as an "internal armed conflict".\n\n On 11 November 2019, on the behalf of the Organization of Islamic Cooperation, The Gambia applied to the Court against Myanmar alleging that Myanmar has committed mass murder; rape and destruction of communities against the Rohingya since October 2016. Now it is the Court\'s time to decide and give a verdict in accordance with the international law and determine the situation of the Rohingya.',
+    image: '',
+    usg: '',
+    acas: '',
+    rop: '',
+    studyguide: ''
+}
 </script>
 
 <style scoped>

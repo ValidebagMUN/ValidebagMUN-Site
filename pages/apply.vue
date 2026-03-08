02d8d24021e80e26ff3199d7221e819cdf07d1b5
HexaBinary <egeanakli+github@pm.me>
2026-03-08 13:52:08 +0300

    refactor: application page unhidden (notice: no functional anchors)

--- pages/apply.vue
+++ pages/apply.vue
@@ -1,33 +1,34 @@
 <template>
     <div class="pt-8 hero text-neutral-content">
         <div class="flex-col hero-content lg:flex-row">
-            <!-- <div class="lg:ml-[-5em]">
+            <div class="lg:ml-[-5em]">
                 <h1 class="text-5xl font-bold mb-6">Application Forms</h1>
                 <div class="flex flex-col gap-4 py-4">
-                    <a href="https://forms.gle/PQhq8KoCTkrKjHXF7" target="_blank" rel="noopener"
+                    <a href="" target="_blank" rel="noopener"
                         class="btn btn-primary text-lg py-6">Apply as a Delegation</a>
-                    <a href="https://forms.gle/xNnVPwGJ6GnKuKnW7" target="_blank" rel="noopener"
+                    <a href="" target="_blank" rel="noopener"
                         class="btn btn-primary text-lg py-6">Apply as an Individual Delegate</a>
-                    <a href="https://forms.gle/sgEHSSr2cbRgFpk86" target="_blank" rel="noopener"
+                    <a href="" target="_blank" rel="noopener"
                         class="btn btn-primary text-lg py-6">Apply as an Admin</a>
-                    <a href="https://forms.gle/S7M3A7ZJxqWmondF7" target="_blank" rel="noopener"
+                    <a href="" target="_blank" rel="noopener"
                         class="btn btn-primary text-lg py-6">Apply as a Press Member</a>
+                    <a href="" target="_blank" rel="noopener"
+                        class="btn btn-primary text-lg py-6">Apply as a PR Member</a>
                 </div>
             </div>
             <div class="divider divider-horizontal max-sm:hidden"></div>
             <div>
                 <h1 class="text-5xl font-bold mb-6 lg:mt-[-1.75em]">Fees</h1>
                 <div class="flex flex-col gap-4 py-4">
-                    <h4 class="text-lg">Delegations: 1275₺ per delegate</h4>
-                    <h4 class="text-lg">Individual Delegates: 1290₺</h4>
-                    <h4 class="text-lg">Admins: 675₺</h4>
-                    <h4 class="text-lg">Press: 650₺</h4>
+                    <h4 class="text-lg">Delegations: 1300₺ per delegate</h4>
+                    <h4 class="text-lg">Individual Delegates: 1350₺</h4>
+                    <h4 class="text-lg">Admins, Press & PR Members: 750₺</h4>
                 </div>
-            </div> -->
-            <div>
+            </div>
+            <!-- <div>
                 <h1 class="text-5xl font-bold mb-6">Applications to ValidebağMUN'25 have been closed.</h1>
                 <h2 class="text-4xl font-bold mb-6">Thank you for your interest.</h2>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>

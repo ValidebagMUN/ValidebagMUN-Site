f16d66ed96c8967a37dbac42905c3cf03fd8280d
HexaBinary <egeanakli+github@pm.me>
2026-03-08 13:40:05 +0300

    refactor: team changes

--- pages/team/directorate.vue
+++ pages/team/directorate.vue
@@ -4,26 +4,21 @@
             <div>
                 <h1 class="my-10 text-5xl font-bold text-center">The Directorate</h1>
                 <div class="container flex flex-col items-center justify-center lg:flex-row">
-                    <TeamCard img="/img/team/ene.jpg" name="Elçin Naz Erkiletlioğlu" title="Director-General" />
-                    <TeamCard img="/img/team/ei.jpg" name="Elif İnetaş" title="Deputy Director-General" />
+                    <TeamCard img="/img/team/iu.jpg" name="İncilay Uysal" title="Director-General" />
+                    <TeamCard img="/img/team/du.jpg" name="Defne Ulkar" title="Deputy Director-General" />
                 </div>
                 <div class="container flex flex-col items-center justify-center lg:flex-row">
-                    <TeamCard img="/img/team/dk.jpg" name="Dila Karadeniz" title="Head of Public Relations" />
-                    <TeamCard img="/img/team/sa.jpg" name="Şevval Yasemin Aslan" title="Co-Head of the Administrative Staff" />
-                    <TeamCard img="/img/team/dc.jpg" name="Demir Çimen" title="Co-Head of the Administrative Staff" />
+                    <TeamCard img="/img/team/nu.jpg" name="Nil Üzüm" title="Head of Public Relations" />
+                    <TeamCard img="/img/team/tt.jpg" name="Tuna Türktarhan" title="Co-Head of the Administrative Staff" />
+                    <TeamCard img="/img/team/oa.jpg" name="Özge Altaş" title="Co-Head of the Administrative Staff" />
                 </div>
                 <div class="container flex flex-col items-center justify-center lg:flex-row">
-                    <TeamCard img="/img/team/ea.jpg" name="Ahmet Emir Alev" title="Co-Head of Information Technologies" />
-                    <TeamCard img="/img/team/ga.jpg" name="Gizem Aral" title="Co-Head of Information Technologies" />
-                    <TeamCard img="/img/team/yb.jpg" name="Yüsra Berk" title="Deputy Head of the Administrative Staff" />
+                    <TeamCard img="/img/team/aka.jpg" name="Alp Karaca Akten" title="Head of Finance" />
+                    <TeamCard img="/img/team/aea.jpg" name="Ahmet Emir Alev" title="Head of Press & Media" />
                 </div>
                 <div class="container flex flex-col items-center justify-center lg:flex-row">
-                    <TeamCard img="/img/team/id.jpg" name="Irmak Dağlıoğlu" title="Co-Head of Logistics" />
-                    <TeamCard img="/img/team/vb.jpg" name="Eymen Vefa Başar" title="Co-Head of Logistics" />
-                </div>
-                <div class="container flex flex-col items-center justify-center lg:flex-row">
-                    <TeamCard img="/img/team/mt.jpg" name="Meva Tunç" title="Co-Head of Press & Media" />
-                    <TeamCard img="/img/team/iu.jpg" name="İncilay Uysal" title="Co-Head of Press & Media" />
+                    <TeamCard img="/img/team/uo.jpg" name="Ufuk Özkul" title="Co-Head of Security" />
+                    <TeamCard img="/img/team/byg.jpg" name="Bayram Yiğit Görgün" title="Co-Head of Security" />
                 </div>
             </div>
         </div>
--- pages/team/secretariat.vue
+++ pages/team/secretariat.vue
@@ -4,12 +4,12 @@
             <div>
                 <h1 class="my-10 text-5xl font-bold text-center">The Secretariat</h1>
                 <div class="container flex flex-col items-center justify-center lg:flex-row">
-                    <TeamCard img="/img/team/zro.jpg" name="Zeynep Rüya Özdemir" title="Secretary-General" />
-                    <TeamCard img="/img/team/ek.jpg" name="Eylül Kalkan" title="Deputy Secretary-General" />
+                    <TeamCard img="/img/team/zbd.jpg" name="Zehra Betül Duysak" title="Secretary-General" />
+                    <TeamCard img="/img/team/yk.jpg" name="Yağmur Kaya" title="Deputy Secretary-General" />
                 </div>
                 <div class="container flex flex-col items-center justify-center lg:flex-row">
-                    <TeamCard img="/img/team/akm.jpg" name="Ali Kağan Menekşelioğlu" title="Head of Academy" />
-                    <TeamCard img="/img/team/ku.jpg" name="Kemal Uzunçam" title="Head of Crisis" />
+                    <TeamCard img="/img/team/rp.jpg" name="Reha Pilanlıoğlu" title="Head of Academy" />
+                    <TeamCard img="/img/team/kk.jpg" name="Kerem Kılıç" title="Head of Crisis" />
                 </div>
             </div>
         </div>
